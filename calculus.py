ecdee1887f96dc673a81b1ee8ef7200c4a45217c
seama107 <a8d4c3bab3dd47c89eb25e4486993de9@compute.c.sage-math-inc.internal>
2016-03-01 18:18:16 +0000

    Created the py file

---
+++ calculus.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""
+Cw5 the pinacle of groupwork and efficienceyc and comroodery
+
+Calculus module implementing
+discrete function function
+differentiation function
+and trapezoidal integration function
+
+"""
